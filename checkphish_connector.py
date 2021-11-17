b5995c8e924d8f7f583d7dd352fcd839f97083e5
Daniel Federschmidt <dfederschmidt@splunk.com>
2021-11-17 10:19:51 +0100

    another attempt to make semgrep happy

--- checkphish_connector.py
+++ checkphish_connector.py
@@ -262,67 +262,50 @@ def main():
     import argparse
 
     import pudb
-
     pudb.set_trace()
 
     argparser = argparse.ArgumentParser()
 
-    argparser.add_argument("input_test_json", help="Input Test JSON file")
-    argparser.add_argument("-u", "--username", help="username", required=False)
-    argparser.add_argument("-p", "--password", help="password", required=False)
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
 
     args = argparser.parse_args()
     session_id = None
 
-    username = args.username
-    password = args.password
-
-    if username is not None and password is None:
-
-        # User specified a username but not a password, so ask
-        import getpass
-
-        password = getpass.getpass("Password: ")
-
-    if username and password:
+    if args.username and args.password:
+        login_url = BaseConnector._get_phantom_base_url() + "login"
         try:
-            login_url = CheckphishConnector._get_phantom_base_url() + "/login"
-
             print("Accessing the Login page")
-            r = requests.get(
-                login_url, verify=False, timeout=10  # nosemgrep
-            )
-            csrftoken = r.cookies["csrftoken"]
-
-            data = dict()
-            data["username"] = username
-            data["password"] = password
-            data["csrfmiddlewaretoken"] = csrftoken
-
-            headers = dict()
-            headers["Cookie"] = "csrftoken=" + csrftoken
-            headers["Referer"] = login_url
+            r = requests.get(login_url, timeout=60)
+            csrftoken = r.cookies['csrftoken']
+            data = {'username': args.username, 'password': args.password, 'csrfmiddlewaretoken': csrftoken}
+            headers = {'Cookie': 'csrftoken={0}'.format(csrftoken), 'Referer': login_url}
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post(
-                login_url, verify=False, data=data, headers=headers, timeout=10  # nosemgrep
-            )
-            session_id = r2.cookies["sessionid"]
+            r2 = requests.post(login_url, data=data, headers=headers, timeout=60)
+            session_id = r2.cookies['sessionid']
+
         except Exception as e:
-            print("Unable to get session id from the platform. Error: " + str(e))
+            print("Unable to get session id from the platform. Error: {0}".format(str(e)))
             sys.exit(1)
 
-    with open(args.input_test_json) as f:
+    if len(sys.argv) < 2:
+        print("No test json specified as input")
+        sys.exit(0)
+
+    with open(sys.argv[1]) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
-        print(json.dumps(in_json, indent=4))
+        print(json.dumps(in_json, indent=' ' * 4))
 
+        # Create the connector class object
         connector = CheckphishConnector()
+
         connector.print_progress_message = True
 
         if session_id is not None:
-            in_json["user_session_token"] = session_id
-            connector._set_csrf_info(csrftoken, headers["Referer"])
+            in_json['user_session_token'] = session_id
 
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
